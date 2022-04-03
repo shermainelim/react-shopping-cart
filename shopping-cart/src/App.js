ad684dad6ff80e371505f431904cf0161d412ffb
shermaine <shermaine.lim.si.hui@gmail.com>
2022-04-03 17:18:36 +0800

    react router link

--- shopping-cart/src/App.js
+++ shopping-cart/src/App.js
@@ -1,16 +1,6 @@
-import { Routes, Route, Outlet } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import Navigation from "./routes/navigation/navigation";
 import Home from "./routes/home/home";
-
-const Navigation = () => {
-  return (
-    <div>
-      <div>
-        <h1>I am the navigation bar</h1>
-      </div>
-      <Outlet />
-    </div>
-  );
-};
 
 const Shop = () => {
   return <h1>I am the shop page</h1>;
