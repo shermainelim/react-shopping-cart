fdcc7bccf3b446d800c31a996a94020079fd3b19
shermaine <shermaine.lim.si.hui@gmail.com>
2022-04-11 18:59:23 +0800

    sign up form pt1

--- shopping-cart/src/routes/sign-in/sign-in.jsx
+++ shopping-cart/src/routes/sign-in/sign-in.jsx
@@ -3,6 +3,8 @@ import {
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase";
 
+import SignUpForm from "../../components/sign-up-form/sign-up-form";
+
 const SignIn = () => {
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
@@ -13,6 +15,7 @@ const SignIn = () => {
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+      <SignUpForm />
     </div>
   );
 };
