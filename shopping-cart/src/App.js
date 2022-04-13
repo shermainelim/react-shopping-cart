151b5edeed033845dcdf1b4c9d7843243ba83220
shermaine <shermaine.lim.si.hui@gmail.com>
2022-04-13 22:20:15 +0800

    sign in form

--- shopping-cart/src/App.js
+++ shopping-cart/src/App.js
@@ -1,7 +1,8 @@
-import { Routes, Route } from "react-router-dom";
-import Navigation from "./routes/navigation/navigation";
-import Home from "./routes/home/home";
-import SignIn from "./routes/sign-in/sign-in";
+import { Routes, Route } from 'react-router-dom';
+
+import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
+import SignIn from './routes/sign-in/sign-in.component';
 
 const Shop = () => {
   return <h1>I am the shop page</h1>;
@@ -10,10 +11,10 @@ const Shop = () => {
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Navigation />}>
+      <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
-        <Route path="shop" element={<Shop />} />
-        <Route path="sign-in" element={<SignIn />} />
+        <Route path='shop' element={<Shop />} />
+        <Route path='sign-in' element={<SignIn />} />
       </Route>
     </Routes>
   );
