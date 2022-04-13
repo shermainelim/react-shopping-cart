a9b8d379b60d0db3fb2a78e92dc1554d63402be7
shermaine <shermaine.lim.si.hui@gmail.com>
2022-04-13 21:38:49 +0800

    custom button component

--- shopping-cart/src/components/sign-up-form/sign-up-form.jsx
+++ shopping-cart/src/components/sign-up-form/sign-up-form.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 
+import "./sign-up-form.scss";
+
 import FormInput from "../form-input/form-input";
 
 import {
   createAuthUserWithEmailAndPassword,
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase";
+import Button from "../button/button";
 
 const defaultFormFields = {
   displayName: "",
@@ -53,8 +56,9 @@ const SignUpForm = () => {
   };
 
   return (
-    <div>
-      <h1>Sign up with your email and password</h1>
+    <div className="sign-up-container">
+      <h2>Don't have an account?</h2>
+      <span>Sign up with your email and password</span>
       <form onSubmit={handleSubmit}>
         <FormInput
           label="Display Name"
@@ -88,7 +92,7 @@ const SignUpForm = () => {
           name="confirmPassword"
           value={confirmPassword}
         />
-        <button type="submit">Sign Up</button>
+        <Button type="submit">Sign Up</Button>
       </form>
     </div>
   );
