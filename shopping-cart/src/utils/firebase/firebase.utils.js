346605e7ef1a9342181a831c5b96ed59832f9072
shermaine <shermaine.lim.si.hui@gmail.com>
2022-04-15 20:56:31 +0800

    Update firebase.utils.js

--- shopping-cart/src/utils/firebase/firebase.utils.js
+++ shopping-cart/src/utils/firebase/firebase.utils.js
@@ -11,12 +11,12 @@ import {
 import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk",
-  authDomain: "crwn-clothing-db-98d4d.firebaseapp.com",
-  projectId: "crwn-clothing-db-98d4d",
-  storageBucket: "crwn-clothing-db-98d4d.appspot.com",
-  messagingSenderId: "626766232035",
-  appId: "1:626766232035:web:506621582dab103a4d08d6",
+  apiKey: "AIzaSyCLWUxGzvUfHfht22eyteBwk1yPYxzxAAs",
+  authDomain: "crwn-clothing-db-4950d.firebaseapp.com",
+  projectId: "crwn-clothing-db-4950d",
+  storageBucket: "crwn-clothing-db-4950d.appspot.com",
+  messagingSenderId: "106542231307",
+  appId: "1:106542231307:web:b55c4963b7de719c263c0e",
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
