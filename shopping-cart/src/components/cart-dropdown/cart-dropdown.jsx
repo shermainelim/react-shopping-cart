4147a7d3488f52918fdabe7491962f70529f2abf
shermaine <shermaine.lim.si.hui@gmail.com>
2022-04-21 23:30:42 +0800

    Cart Icon and Dropdown

---
+++ shopping-cart/src/components/cart-dropdown/cart-dropdown.jsx
@@ -0,0 +1,14 @@
+import Button from "../button/button.component";
+
+import "./cart-downdown.scss";
+
+const CartDropdown = () => {
+  return (
+    <div className="cart-dropdown-container">
+      <div className="cart-items" />
+      <Button>GO TO CHECKOUT</Button>
+    </div>
+  );
+};
+
+export default CartDropdown;
