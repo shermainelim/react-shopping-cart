3c0f377bc1e3b363ac577b7aba0ab9cbf91a18c7
shermaine <shermaine.lim.si.hui@gmail.com>
2022-04-22 11:50:58 +0800

    decrement checkout item qnty

--- shopping-cart/src/routes/checkout/checkout.jsx
+++ shopping-cart/src/routes/checkout/checkout.jsx
@@ -4,7 +4,8 @@ import { CartContext } from "../../contexts/cart.context";
 import { useContext } from "react";
 
 const Checkout = () => {
-  const { cartItems, addItemToCart } = useContext(CartContext);
+  const { cartItems, addItemToCart, removeItemFromCart } =
+    useContext(CartContext);
   return (
     <div>
       <h1>I am the checkout page</h1>
@@ -16,7 +17,9 @@ const Checkout = () => {
               <h2>{name}</h2>
               <span>{quantity}</span>
               <br />
-              <span>decrement</span>
+              <span onClick={() => removeItemFromCart(cartItem)}>
+                decrement
+              </span>
               <br />
               <span onClick={() => addItemToCart(cartItem)}>increment</span>
             </div>
