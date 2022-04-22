1e3ee90b9badb54da98b23d8613f39c6977aea07
shermaine <shermaine.lim.si.hui@gmail.com>
2022-04-22 18:59:46 +0800

    firestore addCollectionAndDocs
    write batch
    - get products and categories from Firestore

--- shopping-cart/src/contexts/products.context.jsx
+++ shopping-cart/src/contexts/products.context.jsx
@@ -1,13 +1,32 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
-import PRODUCTS from "../shop-data.json";
+import {
+  addCollectionAndDocuments,
+  getCategoriesAndDocuments,
+} from "../utils/firebase/firebase.utils.js";
+
+import SHOP_DATA from "../shop-data.js";
 
 export const ProductsContext = createContext({
   products: [],
 });
 
 export const ProductsProvider = ({ children }) => {
-  const [products, setProducts] = useState(PRODUCTS);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments();
+      console.log(categoryMap);
+    };
+
+    getCategoriesMap();
+  }, []);
+
+  // useEffect(() => {
+  //   addCollectionAndDocuments("categories", SHOP_DATA);
+  // }, []);
+
   const value = { products };
   return (
     <ProductsContext.Provider value={value}>
