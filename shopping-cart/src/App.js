5e4b43aecbda3d82ab10d25e1616a38251d616ec
shermaine <shermaine.lim.si.hui@gmail.com>
2022-04-22 11:32:05 +0800

    useNavigate and checkout and increment context

--- shopping-cart/src/App.js
+++ shopping-cart/src/App.js
@@ -4,6 +4,7 @@ import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
 import Authentication from "./routes/authentication/authentication.component";
 import Shop from "./routes/shop/shop";
+import Checkout from "./routes/checkout/checkout";
 
 // const Shoppes = () => {
 //   return (<Shop/>)
@@ -16,6 +17,7 @@ const App = () => {
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
         <Route path="auth" element={<Authentication />} />
+        <Route path="checkout" element={<Checkout />} />
       </Route>
     </Routes>
   );
--- shopping-cart/src/components/cart-dropdown/cart-dropdown.jsx
+++ shopping-cart/src/components/cart-dropdown/cart-dropdown.jsx
@@ -1,4 +1,5 @@
 import Button from "../button/button.component";
+import { useNavigate } from "react-router-dom";
 
 import "./cart-downdown.scss";
 
@@ -10,6 +11,12 @@ import { createUserWithEmailAndPassword } from "firebase/auth";
 
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
+  const navigate = useNavigate();
+
+  const goToCheckoutHandler = () => {
+    navigate("./checkout");
+  };
+
   return (
     <div className="cart-dropdown-container">
       <div className="cart-items">
@@ -17,7 +24,7 @@ const CartDropdown = () => {
           <CartItem key={item.id} cartItem={item} />
         ))}
       </div>
-      <Button>GO TO CHECKOUT</Button>
+      <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
     </div>
   );
 };
